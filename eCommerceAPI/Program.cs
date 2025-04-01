ddd5501115548bf6effd924ee99ce35669c3c9a5
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2025-04-01 12:46:57 +0200

    v2

--- eCommerceAPI/Program.cs
+++ eCommerceAPI/Program.cs
@@ -18,7 +18,7 @@ if (app.Environment.IsDevelopment())
     {
         options.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
         options.RoutePrefix = string.Empty;
-        options.DocumentTitle = "eCommerceAPI-test";
+        options.DocumentTitle = "eCommerceAPI";
         options.EnableDeepLinking();
     });
 }
